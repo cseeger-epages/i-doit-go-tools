c53b7847828963fcdcc6be231364c718eedbc76b
Carsten Seeger <cseeger@epages.com>
2017-08-29 13:02:57 +0200

    improve make.sh

--- make.sh
+++ make.sh
@@ -1,10 +1,22 @@
+#!/bin/bash
+
 if [[ ! -d "bin" ]]; then
   mkdir bin
 fi
 
-for f in ./*/*.go;
-  do
-    echo "build $f"
-    go build $f
-    mv $(basename ${f::-3}) bin/
-done
+if [ "$(grep -R '<apikey>' ./* | wc -l)" -gt "1" ]; then
+  echo "please set proper login information !"
+  echo 'change all goidoit.NewLogin("<i-doit-url>/src/jsonrpc.php", "<apikey>", "<username>", "<password>") to use proper i-doit-url, apikey and username password)'
+  read -p "proceed anyway ? [y/n] " -n 1 -r
+  echo 
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    for f in ./*/*.go;
+      do
+        echo "build $f"
+        go build $f
+        mv $(basename ${f::-3}) bin/
+    done
+  fi
+  exit 1
+fi
+
